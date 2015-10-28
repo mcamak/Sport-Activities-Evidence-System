be0bc7fd28fcc1b06714cb88f9b2f61557141a5b
Marián Čamák <majocamak@gmail.com>
2015-10-28 13:43:37 +0100

    Completed SportActivity interface

--- SportActivityEvidenceSystem/src/main/java/cz/muni/fi/pa165/sportactivityevidencesystem/service/SportActivityService.java
+++ SportActivityEvidenceSystem/src/main/java/cz/muni/fi/pa165/sportactivityevidencesystem/service/SportActivityService.java
@@ -1,14 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cz.muni.fi.pa165.sportactivityevidencesystem.service;
 
+import cz.muni.fi.pa165.sportactivityevidencesystem.entity.SportActivity;
+
 /**
- *
+ * JPA implementation of CRUD operations
  * @author MajoCAM
  */
 public interface SportActivityService {
+    
+    /**
+     * Creates sport activity
+     * @param activity - activity to be created
+     */
+    public void createSportActivity(SportActivity activity);
+
+    /**
+     * Removes sport activity
+     * @param activity - activity to be removed
+     */
+    public void removeSportActivity(SportActivity activity);
+
+    /**
+     * Retrieves sport activity
+     * @param id of the activity, which will be retrieved
+     * @return retrieved activity
+     */
+    public SportActivity findSportActivity(Long id);
+
+    /**
+     * Updates sport activity
+     * @param activity - activity to update
+     */
+    public void updateSportActivity(SportActivity activity);
     
 }
