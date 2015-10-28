c6b321c2c64229796c42673a6cad993704d8cc4c
Marián Čamák <majocamak@gmail.com>
2015-10-28 10:54:57 +0100

    Completed entity class code, added attributes, getters & setters, equals & hashCode

--- SportActivityEvidenceSystem/src/main/java/cz/muni/fi/pa165/sportactivityevidencesystem/entity/ActivityRecord.java
+++ SportActivityEvidenceSystem/src/main/java/cz/muni/fi/pa165/sportactivityevidencesystem/entity/ActivityRecord.java
@@ -5,10 +5,100 @@
  */
 package cz.muni.fi.pa165.sportactivityevidencesystem.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
 /**
  *
  * @author MajoCAM
  */
-public class ActivityRecord {     
+@Entity
+public class ActivityRecord {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private Long timeSeconds;
+    
+    private int distance;
+    
+    @OneToOne
+    private SportActivity activity;
+    
+    @OneToMany
+    private List<User> users = new ArrayList<>();
+
+    public Long getTimeSeconds() {
+        return timeSeconds;
+    }
+
+    public void setTimeSeconds(Long timeSeconds) {
+        this.timeSeconds = timeSeconds;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public SportActivity getActivity() {
+        return activity;
+    }
+
+    public void setActivity(SportActivity activity) {
+        this.activity = activity;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 41 * hash + Objects.hashCode(this.timeSeconds);
+        hash = 41 * hash + this.distance;
+        hash = 41 * hash + Objects.hashCode(this.activity);
+        hash = 41 * hash + Objects.hashCode(this.users);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof ActivityRecord)) {
+            return false;
+        }
+        final ActivityRecord other = (ActivityRecord) obj;
+        if (!Objects.equals(this.timeSeconds, other.timeSeconds)) {
+            return false;
+        }
+        if (this.distance != other.distance) {
+            return false;
+        }
+        if (!Objects.equals(this.activity, other.activity)) {
+            return false;
+        }
+        if (!Objects.equals(this.users, other.users)) {
+            return false;
+        }
+        return true;
+    }
 }
