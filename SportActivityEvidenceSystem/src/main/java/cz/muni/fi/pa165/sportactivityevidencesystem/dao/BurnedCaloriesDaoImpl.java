f123a1c72808587ea27a2204a652d4957dff493b
Tom Effenberger <tomas.effenberger@gmail.com>
2015-10-28 21:02:24 +0100

    Implement BurnedCaloriesDaoImpl

--- SportActivityEvidenceSystem/src/main/java/cz/muni/fi/pa165/sportactivityevidencesystem/dao/BurnedCaloriesDaoImpl.java
+++ SportActivityEvidenceSystem/src/main/java/cz/muni/fi/pa165/sportactivityevidencesystem/dao/BurnedCaloriesDaoImpl.java
@@ -1,9 +1,36 @@
 package cz.muni.fi.pa165.sportactivityevidencesystem.dao;
 
+import cz.muni.fi.pa165.sportactivityevidencesystem.entity.BurnedCalories;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 /**
  *
  * @author Tomas Effenberger
  */
-public class BurnedCaloriesDaoImpl {
-	
+public class BurnedCaloriesDaoImpl implements BurnedCaloriesDao {
+
+	@PersistenceContext
+	private EntityManager em;
+
+	@Override
+	public void createBurnedCalories(BurnedCalories burnedCalories) {
+		em.persist(burnedCalories);
+	}
+
+	@Override
+	public void deleteBurnedCalories(BurnedCalories burnedCalories) {
+		em.remove(burnedCalories);
+	}
+
+	@Override
+	public BurnedCalories getBurnedCalories(Long id) {
+		return em.find(BurnedCalories.class, id);
+	}
+
+	@Override
+	public void updateBurnedCalories(BurnedCalories burnedCalories) {
+		em.merge(burnedCalories);
+	}
+
 }
