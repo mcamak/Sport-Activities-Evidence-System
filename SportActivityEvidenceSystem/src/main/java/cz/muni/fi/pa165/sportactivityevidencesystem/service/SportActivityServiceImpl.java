1f1d67a18a468b305f4eda814565776fdd79ec49
Marián Čamák <majocamak@gmail.com>
2015-10-28 20:10:24 +0100

    Completed SportActivityServiceImpl class - JPA implementation of CRUD opreations

--- SportActivityEvidenceSystem/src/main/java/cz/muni/fi/pa165/sportactivityevidencesystem/service/SportActivityServiceImpl.java
+++ SportActivityEvidenceSystem/src/main/java/cz/muni/fi/pa165/sportactivityevidencesystem/service/SportActivityServiceImpl.java
@@ -5,10 +5,58 @@
  */
 package cz.muni.fi.pa165.sportactivityevidencesystem.service;
 
+import cz.muni.fi.pa165.sportactivityevidencesystem.dao.SportActivityDao;
+import cz.muni.fi.pa165.sportactivityevidencesystem.entity.SportActivity;
+import javax.inject.Inject;
+
 /**
  *
  * @author MajoCAM
  */
 public class SportActivityServiceImpl implements SportActivityService {
+    
+    @Inject
+    private SportActivityDao sportActivityDao;
+
+    @Override
+    public void createSportActivity(SportActivity activity) {
+        if(activity == null) {
+            throw new IllegalArgumentException("Activity is null. ");
+        }
+//        if(activity.getId() != null) {
+//            throw new IllegalArgumentException("Activity ID isn't null. It is already stored in database. ");
+//        }
+        sportActivityDao.createSportActivity(activity);
+    }
+
+    @Override
+    public void removeSportActivity(SportActivity activity) {
+        if(activity == null) {
+            throw new IllegalArgumentException("Activity is null. ");
+        }
+//        if(activity.getId() == null) {
+//            throw new IllegalArgumentException("Activity ID is null - activity isn't stored in database. ");
+//        }
+        sportActivityDao.deleteSportActivity(activity);
+    }
+
+    @Override
+    public SportActivity findSportActivity(Long id) {
+        if(id == null) {
+            throw new IllegalArgumentException("ID is null. ");
+        }
+        return sportActivityDao.findSportActivity(id);
+    }
+
+    @Override
+    public void updateSportActivity(SportActivity activity) {
+        if(activity == null) {
+            throw new IllegalArgumentException("Activity is null. ");
+        }
+//        if(activity.getId() == null) {
+//            throw new IllegalArgumentException("Activity ID is null - activity isn't stored in database. ");
+//        }
+        sportActivityDao.updateSportActivity(activity);
+    }
     
 }
