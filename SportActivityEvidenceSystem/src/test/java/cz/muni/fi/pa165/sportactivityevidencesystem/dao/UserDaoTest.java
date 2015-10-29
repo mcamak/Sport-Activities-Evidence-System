7e407e7752f3aae8f8f68d702f092c80ce3d9500
Tom Effenberger <tomas.effenberger@gmail.com>
2015-10-29 22:02:43 +0100

    Implement UserDaoTest

---
+++ SportActivityEvidenceSystem/src/test/java/cz/muni/fi/pa165/sportactivityevidencesystem/dao/UserDaoTest.java
@@ -0,0 +1,112 @@
+package cz.muni.fi.pa165.sportactivityevidencesystem.dao;
+
+import cz.muni.fi.pa165.sportactivityevidencesystem.PersistenceSampleApplicationContext;
+import cz.muni.fi.pa165.sportactivityevidencesystem.entity.User;
+import cz.muni.fi.pa165.sportactivityevidencesystem.enums.Gender;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.test.context.ContextConfiguration;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ * Tests for UserDaoImpl
+ *
+ * @author Tomas Effenberger
+ */
+@RunWith(MockitoJUnitRunner.class)
+@ContextConfiguration(classes = PersistenceSampleApplicationContext.class)
+@Transactional
+public class UserDaoTest {
+
+	@PersistenceContext
+	public EntityManager em;
+
+	@Inject
+	private UserDao userDao;
+
+	private User user1;
+	private User user2;
+
+	@BeforeMethod
+	public void createUsers() {
+		user1 = new User();
+		user1.setAge(20);
+		user1.setName("Adam");
+		user1.setSex(Gender.MALE);
+		user1.setWeight(70);
+		userDao.createUser(user1);
+
+		user2 = new User();
+		user2.setAge(21);
+		user2.setName("Michael");
+		user2.setSex(Gender.MALE);
+		user2.setWeight(68);
+		userDao.createUser(user2);
+	}
+
+	/**
+	 * Test creating new user.
+	 */
+	@Test
+	public void testCreateUser() {
+		Assert.assertNotNull(user1.getId());
+		Assert.assertNotNull(user2.getId());
+	}
+
+	/**
+	 * Test creating user with null argument
+	 */
+	@Test(expectedExceptions = IllegalArgumentException.class)
+	public void testCreateUserWithNullArgument() {
+		userDao.createUser(null);
+	}
+
+	/**
+	 * Test finding user by ID.
+	 */
+	@Test
+	public void testGetUser() {
+		User user = userDao.getUser(user1.getId());
+		Assert.assertEquals("Adam", user.getName());
+		Assert.assertEquals(20, user.getAge());
+		Assert.assertEquals(Gender.MALE, user.getSex());
+		Assert.assertEquals(70, user.getWeight());
+	}
+
+	/**
+	 * Test deleting user.
+	 */
+	@Test
+	public void testDeleteUser() {
+		Assert.assertNotNull(userDao.getUser(user1.getId()));
+		userDao.deleteUser(user1);
+		Assert.assertNotNull(userDao.getUser(user1.getId()));
+	}
+
+	/**
+	 * Test deleting user without ID.
+	 */
+	@Test(expectedExceptions = IllegalArgumentException.class)
+	public void testDeleteWithoutId() {
+		User user = new User();
+		userDao.deleteUser(user);
+	}
+
+	/**
+	 * Test updating user.
+	 */
+	@Test
+	@Transactional
+	public void testUpdateUser() {
+		user2.setName("John");
+		userDao.updateUser(user2);
+		Assert.assertEquals("John", em.find(User.class, user2.getId()));
+	}
+
+}
