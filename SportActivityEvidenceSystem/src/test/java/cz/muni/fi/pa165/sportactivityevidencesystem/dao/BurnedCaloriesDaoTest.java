52d2c03c66cf2c50c731948ebd5d0e82d484110a
Marián Čamák <majocamak@gmail.com>
2015-10-29 15:37:30 +0100

    Added more test methods.

--- SportActivityEvidenceSystem/src/test/java/cz/muni/fi/pa165/sportactivityevidencesystem/dao/BurnedCaloriesDaoTest.java
+++ SportActivityEvidenceSystem/src/test/java/cz/muni/fi/pa165/sportactivityevidencesystem/dao/BurnedCaloriesDaoTest.java
@@ -4,7 +4,7 @@ import cz.muni.fi.pa165.sportactivityevidencesystem.entity.BurnedCalories;
 import cz.muni.fi.pa165.sportactivityevidencesystem.entity.SportActivity;
 import static org.junit.Assert.*;
 import org.junit.Before;
-import org.junit.Test;
+import org.testng.annotations.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
@@ -28,10 +28,10 @@ public class BurnedCaloriesDaoTest {
     }
     
     /**
-     * Test of createUser method, of class UserDaoImpl.
+     * Test creating burned calory.
      */
     @Test
-    public void testCreateUser() {
+    public void testCreateBurnedCalory() {
         BurnedCalories bCal = new BurnedCalories();
         bCal.setActivity(sportActivity);
         bCal.setBodyWeight(50);
@@ -40,12 +40,59 @@ public class BurnedCaloriesDaoTest {
         burnCalDao.create(bCal);
         assertNotNull(bCal.getId());
     }
+    
+    /**
+     * Test creating burned calory with null calory.
+     */
+    @Test(expectedExceptions=IllegalArgumentException.class)
+    public void testCreateBurnedCaloryWithNullParameter() {        
+        burnCalDao.create(null);
+    }
+    
+    /**
+     * Test creating burned calory with null sport activity.
+     */
+    @Test(expectedExceptions=IllegalArgumentException.class)
+    public void testCreateBurnedCaloryWithNullSportActivity() { 
+        BurnedCalories bCal = new BurnedCalories();
+        bCal.setActivity(null);
+        bCal.setBodyWeight(50);
+        bCal.setCaloriesBurned(50);
+        
+        burnCalDao.create(bCal);
+    }
+    
+    /**
+     * Test creating burned calory with negative body weight.
+     */
+    @Test(expectedExceptions=IllegalArgumentException.class)
+    public void testCreateBurnedCaloryWithNegativeBodyWeight() { 
+        BurnedCalories bCal = new BurnedCalories();
+        bCal.setActivity(sportActivity);
+        bCal.setBodyWeight(-50);
+        bCal.setCaloriesBurned(50);
+        
+        burnCalDao.create(bCal);
+    }
+    
+    /**
+     * Test creating burned calory with negative calories burned.
+     */
+    @Test(expectedExceptions=IllegalArgumentException.class)
+    public void testCreateBurnedCaloryWithNegativeCaloriesBurned() { 
+        BurnedCalories bCal = new BurnedCalories();
+        bCal.setActivity(sportActivity);
+        bCal.setBodyWeight(50);
+        bCal.setCaloriesBurned(-50);
+        
+        burnCalDao.create(bCal);
+    }
 
     /**
-     * Test of deleteUser method, of class UserDaoImpl.
+     * Test deleting burned category.
      */
     @Test
-    public void testDeleteUser() {
+    public void testDeleteBurnedCalory() {
         BurnedCalories bCal = new BurnedCalories();
         bCal.setActivity(sportActivity);
         bCal.setBodyWeight(50);
@@ -57,12 +104,33 @@ public class BurnedCaloriesDaoTest {
         burnCalDao.delete(bCal);
         assertNull(burnCalDao.findById(bCal.getId()));
     }
+    
+    /**
+     * Test deleting burned category with null category .
+     */
+    @Test(expectedExceptions=IllegalArgumentException.class)
+    public void testDeleteBurnedCaloryWithNullCategory() {
+        burnCalDao.delete(null);
+    }
+    
+    /**
+     * Test deleting burned category with null category id.
+     */
+    @Test(expectedExceptions=IllegalArgumentException.class)
+    public void testDeleteBurnedCaloryWithNullId() {
+        BurnedCalories bCal = new BurnedCalories();
+        bCal.setActivity(sportActivity);
+        bCal.setBodyWeight(50);
+        bCal.setCaloriesBurned(50);
+                
+        burnCalDao.delete(bCal);
+    }
 
     /**
-     * Test of getUser method, of class UserDaoImpl.
+     * Test finding burned category.
      */
     @Test
-    public void testGetUser() {
+    public void testFindBurnedCategory() {
         BurnedCalories bCal = new BurnedCalories();
         bCal.setActivity(sportActivity);
         bCal.setBodyWeight(50);
@@ -71,12 +139,28 @@ public class BurnedCaloriesDaoTest {
         burnCalDao.create(bCal);
         assertNotNull(burnCalDao.findById(bCal.getId()));
     }
+    
+    /**
+     * Test finding burned category with null ID.
+     */
+    @Test(expectedExceptions=IllegalArgumentException.class)
+    public void testFindBurnedCategoryWithNullId() {
+        BurnedCalories bCal = new BurnedCalories();
+        bCal.setActivity(sportActivity);
+        bCal.setBodyWeight(50);
+        bCal.setCaloriesBurned(50);
+        
+        burnCalDao.create(bCal);
+        assertNotNull(burnCalDao.findById(bCal.getId()));
+        
+        burnCalDao.findById(null);
+    }
 
     /**
-     * Test of updateUser method, of class UserDaoImpl.
+     * Test updating burned category.
      */
     @Test
-    public void testUpdateUser() {
+    public void testUpdateBurnedCategory() {
         BurnedCalories bCal = new BurnedCalories();
         bCal.setActivity(sportActivity);
         bCal.setBodyWeight(50);
@@ -92,6 +176,81 @@ public class BurnedCaloriesDaoTest {
         BurnedCalories found = burnCalDao.findById(bCal.getId());
         assertEquals(70, found.getBodyWeight());
         assertEquals(40, found.getCaloriesBurned());        
+    }
+    
+    /**
+     * Test updating burned category with null category.
+     */
+    @Test(expectedExceptions=IllegalArgumentException.class)
+    public void testUpdateBurnedCategoryWithNullCategory() {
+        burnCalDao.update(null);      
+    }
+    
+    /**
+     * Test updating burned category with null category id.
+     */
+    @Test(expectedExceptions=IllegalArgumentException.class)
+    public void testUpdateBurnedCategoryWithNullCategoryId() {
+        BurnedCalories bCal = new BurnedCalories();
+        bCal.setActivity(sportActivity);        
+        bCal.setBodyWeight(70);
+        bCal.setCaloriesBurned(40);
+        burnCalDao.update(bCal);      
+    }
+    
+    /**
+     * Test updating burned category with null sport activity.
+     */
+    @Test(expectedExceptions=IllegalArgumentException.class)
+    public void testUpdateBurnedCategoryWithNullSportActivity() {
+        BurnedCalories bCal = new BurnedCalories();
+        bCal.setActivity(sportActivity);
+        bCal.setBodyWeight(50);
+        bCal.setCaloriesBurned(50);
+        
+        burnCalDao.create(bCal);
+        assertNotNull(burnCalDao.findById(bCal.getId()));
+        
+        bCal.setActivity(null);
+        bCal.setBodyWeight(70);
+        bCal.setCaloriesBurned(40);
+        burnCalDao.update(bCal);      
+    }
+    
+    /**
+     * Test updating burned category with negative body weight.
+     */
+    @Test(expectedExceptions=IllegalArgumentException.class)
+    public void testUpdateBurnedCategoryWithNegativeBodyWeight() {
+        BurnedCalories bCal = new BurnedCalories();
+        bCal.setActivity(sportActivity);
+        bCal.setBodyWeight(50);
+        bCal.setCaloriesBurned(50);
+        
+        burnCalDao.create(bCal);
+        assertNotNull(burnCalDao.findById(bCal.getId()));
+        
+        bCal.setBodyWeight(-70);
+        bCal.setCaloriesBurned(40);
+        burnCalDao.update(bCal);        
+    }
+    
+    /**
+     * Test updating burned category with negative calories burned.
+     */
+    @Test(expectedExceptions=IllegalArgumentException.class)
+    public void testUpdateBurnedCategoryWithNegativeCaloriesBurned() {
+        BurnedCalories bCal = new BurnedCalories();
+        bCal.setActivity(sportActivity);
+        bCal.setBodyWeight(50);
+        bCal.setCaloriesBurned(50);
+        
+        burnCalDao.create(bCal);
+        assertNotNull(burnCalDao.findById(bCal.getId()));
+        
+        bCal.setBodyWeight(70);
+        bCal.setCaloriesBurned(-40);
+        burnCalDao.update(bCal);       
     }
     
 }
