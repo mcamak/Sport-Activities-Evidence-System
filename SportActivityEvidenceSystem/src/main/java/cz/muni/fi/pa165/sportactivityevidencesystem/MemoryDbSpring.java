bb98acaea4da2bbabf19a7434936f805590c503e
Marián Čamák <majocamak@gmail.com>
2015-10-29 16:23:41 +0100

    Added only for test purpose, maybe I remove it in the future

---
+++ SportActivityEvidenceSystem/src/main/java/cz/muni/fi/pa165/sportactivityevidencesystem/MemoryDbSpring.java
@@ -0,0 +1,19 @@
+package cz.muni.fi.pa165.sportactivityevidencesystem;
+
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+@Configuration
+public class MemoryDbSpring {
+
+    @Bean
+    public DataSource db() {
+        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+        EmbeddedDatabase db = builder.setType(EmbeddedDatabaseType.DERBY).build();
+        return db;
+    }
+}
