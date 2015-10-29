af33836caf3df4006956268ba7a8d2bf0caf50e2
Tom Effenberger <tomas.effenberger@gmail.com>
2015-10-29 22:18:01 +0100

    Add annotations to User class

--- SportActivityEvidenceSystem/src/main/java/cz/muni/fi/pa165/sportactivityevidencesystem/entity/User.java
+++ SportActivityEvidenceSystem/src/main/java/cz/muni/fi/pa165/sportactivityevidencesystem/entity/User.java
@@ -7,98 +7,103 @@ package cz.muni.fi.pa165.sportactivityevidencesystem.entity;
 
 import cz.muni.fi.pa165.sportactivityevidencesystem.enums.Gender;
 import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 /**
  *
- * @author Barbora B.
- * Entity of user - name, age, weight and sex
+ * @author Barbora B. Entity of user - name, age, weight and sex
  */
+@Entity
 public class User {
-    
-    private Long id;
-    private String name; 
-    private Gender sex;
-    private int age;
-    private int weight;
 
-    public Long getId() {
-        return id;
-    }
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	private Gender sex;
+	private int age;
+	private int weight;
 
-    public String getName() {
-        return name;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public Gender getSex() {
-        return sex;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public int getAge() {
-        return age;
-    }
+	public Gender getSex() {
+		return sex;
+	}
 
-    public int getWeight() {
-        return weight;
-    }
+	public int getAge() {
+		return age;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public int getWeight() {
+		return weight;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public void setSex(Gender sex) {
-        this.sex = sex;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setAge(int age) {
-        this.age = age;
-    }
+	public void setSex(Gender sex) {
+		this.sex = sex;
+	}
 
-    public void setWeight(int weight) {
-        this.weight = weight;
-    }
+	public void setAge(int age) {
+		this.age = age;
+	}
 
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 97 * hash + Objects.hashCode(this.id);
-        hash = 97 * hash + Objects.hashCode(this.name);
-        hash = 97 * hash + Objects.hashCode(this.sex);
-        hash = 97 * hash + Objects.hashCode(this.age);
-        hash = 97 * hash + Objects.hashCode(this.weight);
-        return hash;
-    }
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final User other = (User) obj;
-        if (!Objects.equals(this.id, other.id)) {
-            return false;
-        }
-        if (!Objects.equals(this.name, other.name)) {
-            return false;
-        }
-        if (this.sex != other.sex) {
-            return false;
-        }
-        if (!Objects.equals(this.age, other.age)) {
-            return false;
-        }
-        if (!Objects.equals(this.weight, other.weight)) {
-            return false;
-        }
-        return true;
-    }
-    
-    
-    
+	@Override
+	public int hashCode() {
+		int hash = 7;
+		hash = 97 * hash + Objects.hashCode(this.id);
+		hash = 97 * hash + Objects.hashCode(this.name);
+		hash = 97 * hash + Objects.hashCode(this.sex);
+		hash = 97 * hash + Objects.hashCode(this.age);
+		hash = 97 * hash + Objects.hashCode(this.weight);
+		return hash;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		final User other = (User) obj;
+		if (!Objects.equals(this.id, other.id)) {
+			return false;
+		}
+		if (!Objects.equals(this.name, other.name)) {
+			return false;
+		}
+		if (this.sex != other.sex) {
+			return false;
+		}
+		if (!Objects.equals(this.age, other.age)) {
+			return false;
+		}
+		if (!Objects.equals(this.weight, other.weight)) {
+			return false;
+		}
+		return true;
+	}
+
 }
