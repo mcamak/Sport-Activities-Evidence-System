aea106775843d6657cb323644944f2c1b5de37d1
Marián Čamák <majocamak@gmail.com>
2015-11-07 14:36:00 +0100

    Added parameter validity check

--- SportActivityEvidenceSystem/src/main/java/cz/muni/fi/pa165/sportactivityevidencesystem/dao/BurnedCaloriesDaoImpl.java
+++ SportActivityEvidenceSystem/src/main/java/cz/muni/fi/pa165/sportactivityevidencesystem/dao/BurnedCaloriesDaoImpl.java
@@ -4,42 +4,82 @@ import cz.muni.fi.pa165.sportactivityevidencesystem.entity.BurnedCalories;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
 
 /**
  * Implementation of BurnedCaloriesDao
  *
  * @author Tomas Effenberger
  */
+@Repository
 public class BurnedCaloriesDaoImpl implements BurnedCaloriesDao {
 
-	@PersistenceContext
-	private EntityManager em;
+    @PersistenceContext
+    private EntityManager em;
 
-	@Override
-	public void create(BurnedCalories burnedCalories) {
-		em.persist(burnedCalories);
-	}
+    @Override
+    public void create(BurnedCalories burnedCalories) {
+        if(burnedCalories == null) {
+            throw new IllegalArgumentException("BurnedCalories is null. ");
+        }
+        if(burnedCalories.getId() != null) {
+            throw new IllegalArgumentException("BurnedCalories's ID is not null, object is already persisted in DB. ");
+        }
+        if(burnedCalories.getActivity() == null) {
+            throw new IllegalArgumentException("BurnedCalories's sport activity is null. ");
+        }
+        if(burnedCalories.getBodyWeight() < 0) {
+            throw new IllegalArgumentException("BurnedCalories's body weight is negative. ");
+        }
+        if(burnedCalories.getCaloriesBurned() < 0) {
+            throw new IllegalArgumentException("BurnedCalories's calories burned is negative. ");
+        }
+        em.persist(burnedCalories);
+    }
 
-	@Override
-	public void delete(BurnedCalories burnedCalories) {
-		em.remove(burnedCalories);
-	}
+    @Override
+    public void delete(BurnedCalories burnedCalories) {
+        if(burnedCalories == null) {
+            throw new IllegalArgumentException("BurnedCalories is null. ");
+        }
+        if(burnedCalories.getId() == null) {
+            throw new IllegalArgumentException("BurnedCalories's ID is null, object isn't persisted in DB. ");
+        }
+        em.remove(burnedCalories);
+    }
 
-	@Override
-	public BurnedCalories findById(Long id) {
-		return em.find(BurnedCalories.class, id);
-	}
+    @Override
+    public BurnedCalories findById(Long id) {
+        if(id == null) {
+            throw new IllegalArgumentException("ID is null. ");
+        }
+        return em.find(BurnedCalories.class, id);
+    }
 
-	@Override
-	public void update(BurnedCalories burnedCalories) {
-		em.merge(burnedCalories);
-	}
+    @Override
+    public void update(BurnedCalories burnedCalories) {
+        if(burnedCalories == null) {
+            throw new IllegalArgumentException("BurnedCalories is null. ");
+        }
+        if(burnedCalories.getId() == null) {
+            throw new IllegalArgumentException("BurnedCalories's ID is null, first create object. ");
+        }
+        if(burnedCalories.getActivity() == null) {
+            throw new IllegalArgumentException("BurnedCalories's sport activity is null. ");
+        }
+        if(burnedCalories.getBodyWeight() < 0) {
+            throw new IllegalArgumentException("BurnedCalories's body weight is negative. ");
+        }
+        if(burnedCalories.getCaloriesBurned() < 0) {
+            throw new IllegalArgumentException("BurnedCalories's calories burned is negative. ");
+        }
+        em.merge(burnedCalories);
+    }
 
-	@Override
-	public List<BurnedCalories> findAll() {
-		return em.createQuery("SELECT b FROM BurnedCalories b",
-		    BurnedCalories.class)
-		    .getResultList();
-	}
-
+    @Override
+    public List<BurnedCalories> findAll() {
+        return em.createQuery("SELECT b FROM BurnedCalories b",
+                BurnedCalories.class)
+                .getResultList();
+    }
 }
