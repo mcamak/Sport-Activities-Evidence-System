8b8dbeb871ae2d081beed57c5ceb917235a7babc
Marián Čamák <majocamak@gmail.com>
2015-11-08 12:19:21 +0100

    Only formatted code text

--- SportActivityEvidenceSystem/src/main/java/cz/muni/fi/pa165/sportactivityevidencesystem/dao/UserDaoImpl.java
+++ SportActivityEvidenceSystem/src/main/java/cz/muni/fi/pa165/sportactivityevidencesystem/dao/UserDaoImpl.java
@@ -12,40 +12,39 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public class UserDaoImpl implements UserDao {
-    
+
     @PersistenceContext
     private EntityManager em;
 
     @Override
     public void createUser(User user) {
-        if(user == null) {
+        if (user == null) {
             throw new IllegalArgumentException("User is null. ");
         }
-        if(user.getId() != null) {
+        if (user.getId() != null) {
             throw new IllegalArgumentException("User ID is not null. User is already stored in DB. ");
         }
-        if(user.getName() == null || user.getName().isEmpty()) {
+        if (user.getName() == null || user.getName().isEmpty()) {
             throw new IllegalArgumentException("User name is null or empty. ");
         }
-        if(user.getAge() < 0) {
+        if (user.getAge() < 0) {
             throw new IllegalArgumentException("User age is negative. ");
         }
-        if(user.getSex() == null) {
+        if (user.getSex() == null) {
             throw new IllegalArgumentException("Gender of user is null. ");
         }
-        if(user.getWeight() < 0) {
+        if (user.getWeight() < 0) {
             throw new IllegalArgumentException("User bodyweight is lower than zero. ");
         }
-        
         em.persist(user);
     }
 
     @Override
     public void deleteUser(User user) {
-        if(user == null) {
+        if (user == null) {
             throw new IllegalArgumentException("User is null. ");
         }
-        if(user.getId() == null) {
+        if (user.getId() == null) {
             throw new IllegalArgumentException("User ID is null. User is not stored in DB. ");
         }
         em.remove(user);
@@ -53,12 +52,12 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public User findUser(Long id) {
-        if(id == null) {
+        if (id == null) {
             throw new IllegalArgumentException("ID is null. ");
         }
         return em.find(User.class, id);
     }
-    
+
     @Override
     public Collection<User> findAllUsers() {
         return em.createQuery("SELECT u FROM User u", User.class).getResultList();
@@ -66,22 +65,22 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public void updateUser(User user) {
-        if(user == null) {
+        if (user == null) {
             throw new IllegalArgumentException("User is null. ");
         }
-        if(user.getId() == null) {
+        if (user.getId() == null) {
             throw new IllegalArgumentException("User ID is null. First create an user. ");
         }
-        if(user.getName() == null || user.getName().trim().isEmpty()) {
+        if (user.getName() == null || user.getName().trim().isEmpty()) {
             throw new IllegalArgumentException("User name is null or empty. ");
         }
-        if(user.getAge() < 0) {
+        if (user.getAge() < 0) {
             throw new IllegalArgumentException("User age is negative. ");
         }
-        if(user.getSex() == null) {
+        if (user.getSex() == null) {
             throw new IllegalArgumentException("Gender of user is null. ");
         }
-        if(user.getWeight() < 0) {
+        if (user.getWeight() < 0) {
             throw new IllegalArgumentException("User bodyweight is lower than zero. ");
         }
         em.merge(user);
