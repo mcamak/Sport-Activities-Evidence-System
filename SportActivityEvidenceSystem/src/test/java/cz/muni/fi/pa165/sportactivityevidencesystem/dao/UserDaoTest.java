1b269292e60691a2ad03d745913a9090dd1efbde
Marián Čamák <majocamak@gmail.com>
2015-11-08 12:48:49 +0100

    Minor bug fix

--- SportActivityEvidenceSystem/src/test/java/cz/muni/fi/pa165/sportactivityevidencesystem/dao/UserDaoTest.java
+++ SportActivityEvidenceSystem/src/test/java/cz/muni/fi/pa165/sportactivityevidencesystem/dao/UserDaoTest.java
@@ -3,6 +3,7 @@ package cz.muni.fi.pa165.sportactivityevidencesystem.dao;
 import cz.muni.fi.pa165.sportactivityevidencesystem.SportActivitySystemApplicationContext;
 import cz.muni.fi.pa165.sportactivityevidencesystem.entity.User;
 import cz.muni.fi.pa165.sportactivityevidencesystem.enums.Gender;
+import java.util.List;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
