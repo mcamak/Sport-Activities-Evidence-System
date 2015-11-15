bcb27248f99c3c25a066e61d318e44a95746aa1d
Marián Čamák <majocamak@gmail.com>
2015-11-16 00:35:09 +0100

    Added DTO class

---
+++ SAES-api/src/main/java/cz/muni/fi/pa165/dto/SportActivityDTO.java
@@ -0,0 +1,52 @@
+package cz.muni.fi.pa165.dto;
+
+/**
+ *
+ * @author MajoCAM
+ */
+public class SportActivityDTO {
+    
+    private Long id;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+        
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        SportActivityDTO other = (SportActivityDTO) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
+    }    
+}
