ebbbe80ddb6b9579d5c81a8073a6338604223a99
Marián Čamák <majocamak@gmail.com>
2015-11-15 13:24:14 +0100

    Added API exception

---
+++ SAES-api/src/main/java/exceptions/SaesServiceException.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exceptions;
+
+/**
+ *
+ * @author MajoCAM
+ */
+public class SaesServiceException extends RuntimeException {
+
+    public SaesServiceException() {
+    }
+
+    public SaesServiceException(String message) {
+        super(message);
+    }
+
+    public SaesServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public SaesServiceException(Throwable cause) {
+        super(cause);
+    }
+    
+}
