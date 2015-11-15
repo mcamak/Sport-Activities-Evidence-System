36260a6c2ce67ee0790f1638d6b232fecb64c511
Marián Čamák <majocamak@gmail.com>
2015-11-16 00:34:51 +0100

    Completed facade interface

--- SAES-api/src/main/java/cz/muni/fi/pa165/facade/SportActivityFacade.java
+++ SAES-api/src/main/java/cz/muni/fi/pa165/facade/SportActivityFacade.java
@@ -1,9 +1,16 @@
 package cz.muni.fi.pa165.facade;
 
+import cz.muni.fi.pa165.dto.SportActivityDTO;
+import java.util.List;
+
 /**
  *
  * @author MajoCAM
  */
 public interface SportActivityFacade {
-    
+    public Long createSportActivity(SportActivityDTO createDTO);
+    public void changeActivityName(SportActivityDTO old, String newName);
+    public void deleteActivity(Long activityId);
+    public List<SportActivityDTO> getAllActivities();
+    public SportActivityDTO getActivityWithId(Long id);
 }
