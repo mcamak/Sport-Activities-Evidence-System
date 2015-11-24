59641f0df65622b7b07d5bd9898bffad866452b9
Barborka <Barborka@Barborka-NB>
2015-11-24 11:13:08 +0100

    Created ActivityRecordDTO (not final version)

---
+++ SAES-api/src/main/java/cz/muni/fi/pa165/dto/ActivityRecordDTO.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.dto;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author Barborka
+ */
+public class ActivityRecordDTO {
+    
+    private Long id;
+
+    private Long timeSeconds;
+
+    private int distance;
+
+    @ManyToOne
+    private SportActivity activity;
+
+    @OneToMany
+    private final Set<User> users = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getTimeSeconds() {
+        return timeSeconds;
+    }
+
+    public void setTimeSeconds(Long timeSeconds) {
+        this.timeSeconds = timeSeconds;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public SportActivity getActivity() {
+        return activity;
+    }
+
+    public void setActivity(SportActivity activity) {
+        this.activity = activity;
+    }
+
+    public Set<User> getUsers() {
+        return Collections.unmodifiableSet(users);
+    }
+
+    public void addUser(User user) {
+        this.users.add(user);
+    }
+
+    public void removeUser(User user) {
+        this.users.remove(user);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 41 * hash + Objects.hashCode(this.timeSeconds);
+        hash = 41 * hash + this.distance;
+        hash = 41 * hash + Objects.hashCode(this.activity);
+        hash = 41 * hash + Objects.hashCode(this.users);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof ActivityRecordDTO)) {
+            return false;
+        }
+        final ActivityRecordDTO other = (ActivityRecordDTO) obj;
+        if (!Objects.equals(this.timeSeconds, other.getTimeSeconds())) {
+            return false;
+        }
+        if (this.distance != other.getDistance()) {
+            return false;
+        }
+        if (!Objects.equals(this.activity, other.getActivity())) {
+            return false;
+        }
+        if (!Objects.equals(this.users, other.getUsers())) {
+            return false;
+        }
+        return true;
+    }
+    
+}
