c34370221dab7b91b88094176f4f66046fd0a1c3
Barborka <Barborka@Barborka-NB>
2015-11-24 17:24:21 +0100

    SportActivityServiceFacadeTest created

---
+++ SAES-service/src/test/java/cz/fi/muni/fi/pa165/service/facade/SportActivityFacadeTest.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.fi.muni.fi.pa165.service.facade;
+
+import cz.muni.fi.pa165.dto.SportActivityDTO;
+import cz.muni.fi.pa165.facade.SportActivityFacade;
+import cz.muni.fi.pa165.saes.entity.SportActivity;
+import javax.inject.Inject;
+import org.mockito.InjectMocks;
+import org.testng.annotations.Test;
+
+
+
+/**
+ *
+ * @author B. Bajtosova
+ */
+
+public class SportActivityFacadeTest {
+    
+ 
+    
+ 
+}
