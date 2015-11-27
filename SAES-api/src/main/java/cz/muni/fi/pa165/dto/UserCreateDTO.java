2b301e7f7d8be7d55235a4fcb68685fba422e3ba
Marián Čamák <majocamak@gmail.com>
2015-11-27 14:21:08 +0100

    Added lombok getters, setters and eq&hc

--- SAES-api/src/main/java/cz/muni/fi/pa165/dto/UserCreateDTO.java
+++ SAES-api/src/main/java/cz/muni/fi/pa165/dto/UserCreateDTO.java
@@ -1,15 +1,20 @@
 package cz.muni.fi.pa165.dto;
 
 import enums.Gender;
-import java.util.Objects;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * @author MajoCAM
  */
+@Getter
+@Setter
+@EqualsAndHashCode
 public class UserCreateDTO {
 
     @NotNull
@@ -26,70 +31,4 @@ public class UserCreateDTO {
     @Min(0)
     @Max(150)
     Integer age;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Gender getSex() {
-        return sex;
-    }
-
-    public void setSex(Gender sex) {
-        this.sex = sex;
-    }
-
-    public Integer getWeight() {
-        return weight;
-    }
-
-    public void setWeight(Integer weight) {
-        this.weight = weight;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 3;
-        hash = 59 * hash + Objects.hashCode(this.name);
-        hash = 59 * hash + Objects.hashCode(this.sex);
-        hash = 59 * hash + this.weight;
-        hash = 59 * hash + this.age;
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final UserCreateDTO other = (UserCreateDTO) obj;
-        if (!Objects.equals(this.name, other.name)) {
-            return false;
-        }
-        if (this.sex != other.sex) {
-            return false;
-        }
-        if (this.weight != other.weight) {
-            return false;
-        }
-        if (this.age != other.age) {
-            return false;
-        }
-        return true;
-    }
 }
