92987ea9d83feeb906105750b2f7e0d863d8a22e
Jan Shýbal <jan@shybal.eu>
2015-11-27 12:40:34 +0100

    Created getters/setters for BurnedCaloriesDTO.

--- SAES-api/src/main/java/cz/muni/fi/pa165/dto/BurnedCaloriesDTO.java
+++ SAES-api/src/main/java/cz/muni/fi/pa165/dto/BurnedCaloriesDTO.java
@@ -10,5 +10,81 @@ package cz.muni.fi.pa165.dto;
  * @author Jan S.
  */
 public class BurnedCaloriesDTO {
-    
+	
+    private Long id;
+    private SportActivityDTO activity;    
+    private int bodyWeight;    
+    private int caloriesBurned;
+
+    /**
+     * Gets ID.
+     * 
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * Gets activity.
+     * 
+     * @return the activity
+     */
+    public SportActivityDTO getActivity() {
+        return activity;
+    }
+
+    /**
+     * Gets body weight.
+     * 
+     * @return the bodyWeight
+     */
+    public int getBodyWeight() {
+        return bodyWeight;
+    }
+
+    /**
+     * Gets amount of burned calories.
+     * 
+     * @return the caloriesBurned
+     */
+    public int getCaloriesBurned() {
+        return caloriesBurned;
+    }
+
+    /**
+     * Sets ID.
+     * 
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * Sets activity.
+     * 
+     * @param activity the activity to set
+     */
+    public void setActivity(SportActivityDTO activity) {
+        this.activity = activity;
+    }
+
+    /**
+     * Sets body weight.
+     * 
+     * @param bodyWeight the bodyWeight to set
+     */
+    public void setBodyWeight(int bodyWeight) {
+        this.bodyWeight = bodyWeight;
+    }
+
+    /**
+     * Sets the amount of burned calories.
+     * 
+     * @param caloriesBurned the caloriesBurned to set
+     */
+    public void setCaloriesBurned(int caloriesBurned) {
+        this.caloriesBurned = caloriesBurned;
+    }
 }
