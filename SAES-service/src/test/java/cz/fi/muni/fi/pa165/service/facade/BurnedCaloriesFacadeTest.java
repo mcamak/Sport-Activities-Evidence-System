7beceb40a4d895949949f1fbcdaeb202c016d09b
Jan Shýbal <jan@shybal.eu>
2015-11-27 22:32:19 +0100

    Inroduced BurnedCaloriesFacadeTest.

---
+++ SAES-service/src/test/java/cz/fi/muni/fi/pa165/service/facade/BurnedCaloriesFacadeTest.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.fi.muni.fi.pa165.service.facade;
+
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Jan S.
+ */
+public class BurnedCaloriesFacadeTest {
+
+    @Test
+    public void testCreate() {
+
+    }
+
+    @Test
+    public void testFindById() {
+
+    }
+
+    @Test
+    public void testUpdate() {
+
+    }
+
+    @Test
+    public void testDelete() {
+
+    }
+
+}
