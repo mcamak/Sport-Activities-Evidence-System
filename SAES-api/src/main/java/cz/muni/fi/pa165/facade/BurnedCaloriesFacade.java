c29c55beed7646476c8ed98609fabef79b3755f8
Jan Shýbal <jan@shybal.eu>
2015-11-27 20:06:30 +0100

    BurnedCaloriesFacade interface: extended.

--- SAES-api/src/main/java/cz/muni/fi/pa165/facade/BurnedCaloriesFacade.java
+++ SAES-api/src/main/java/cz/muni/fi/pa165/facade/BurnedCaloriesFacade.java
@@ -5,10 +5,41 @@
  */
 package cz.muni.fi.pa165.facade;
 
+import cz.muni.fi.pa165.dto.BurnedCaloriesDTO;
+
 /**
  *
  * @author Jan S.
  */
-public class BurnedCaloriesFacade {
-    
+public interface BurnedCaloriesFacade {
+
+    /**
+     * Creates burned calories.
+     *
+     * @param burnedCalories
+     */
+    public void create(BurnedCaloriesDTO burnedCalories);
+
+    /**
+     * Find burned calories by id.
+     *
+     * @param id
+     * @return burned calories with given id
+     */
+    public BurnedCaloriesDTO findById(Long id);
+
+    /**
+     * Update burned calories.
+     *
+     * @param burnedCalories
+     */
+    public void update(BurnedCaloriesDTO burnedCalories);
+
+    /**
+     * Deletes burned calories.
+     *
+     * @param burnedCalories
+     */
+    public void delete(BurnedCaloriesDTO burnedCalories);
+
 }
