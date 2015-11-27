2c1f651b139518b9c669500106107f82d5206701
Jan Shýbal <jan@shybal.eu>
2015-11-27 21:45:02 +0100

    Implemented BurnedCaloriesFacadeImpl.

--- SAES-service/src/main/java/cz/muni/fi/pa165/service/facade/BurnedCaloriesFacadeImpl.java
+++ SAES-service/src/main/java/cz/muni/fi/pa165/service/facade/BurnedCaloriesFacadeImpl.java
@@ -5,10 +5,47 @@
  */
 package cz.muni.fi.pa165.service.facade;
 
+import cz.muni.fi.pa165.dto.BurnedCaloriesCreateDTO;
+import cz.muni.fi.pa165.dto.BurnedCaloriesDTO;
+import cz.muni.fi.pa165.facade.BurnedCaloriesFacade;
+import cz.muni.fi.pa165.saes.entity.BurnedCalories;
+import cz.muni.fi.pa165.service.BurnedCaloriesService;
+import cz.muni.fi.pa165.service.mapping.BeanMappingService;
+import javax.inject.Inject;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 /**
  *
  * @author Jan S.
  */
-public class BurnedCaloriesFacadeImpl {
-    
+@Service
+@Transactional
+public class BurnedCaloriesFacadeImpl implements BurnedCaloriesFacade {
+
+    @Inject
+    private BurnedCaloriesService burnedCaloriesService;
+
+    @Inject
+    private BeanMappingService bms;
+
+    @Override
+    public void create(BurnedCaloriesCreateDTO burnedCalories) {
+        burnedCaloriesService.create(bms.mapTo(burnedCalories, BurnedCalories.class));
+    }
+
+    @Override
+    public BurnedCaloriesDTO findById(Long id) {
+        return bms.mapTo(burnedCaloriesService.findById(id), BurnedCaloriesDTO.class);
+    }
+
+    @Override
+    public void update(BurnedCaloriesDTO burnedCalories) {
+        burnedCaloriesService.update(bms.mapTo(burnedCalories, BurnedCalories.class));
+    }
+
+    @Override
+    public void delete(BurnedCaloriesDTO burnedCalories) {
+        burnedCaloriesService.delete(bms.mapTo(burnedCalories, BurnedCalories.class));
+    }
 }
