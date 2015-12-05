0539206a3b7024104e8cb23076f0b95bf550fa42
Marián Čamák <majocamak@gmail.com>
2015-12-05 22:41:39 +0100

    Added subclass of DataAccessException

---
+++ SAES-service/src/main/java/exceptions/SaesDataAccessException.java
@@ -0,0 +1,22 @@
+package exceptions;
+
+import org.springframework.dao.DataAccessException;
+
+/**
+ * Subclass of DataAccessException.
+ * @author MajoCAM
+ */
+public class SaesDataAccessException extends DataAccessException {
+
+    public SaesDataAccessException(String msg) {
+        super(msg);
+    }
+    
+    public SaesDataAccessException(Throwable cause) {
+        super("", cause);
+    }
+    
+    public SaesDataAccessException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+}
