8fca41c468e1d8e973a8aa9bb9884475941938af
Marián Čamák <majocamak@gmail.com>
2015-12-05 21:27:13 +0100

    Added javadoc

--- SAES-api/src/main/java/cz/muni/fi/pa165/facade/UserFacade.java
+++ SAES-api/src/main/java/cz/muni/fi/pa165/facade/UserFacade.java
@@ -10,15 +10,47 @@ import java.util.List;
  */
 public interface UserFacade {
 
+    /**
+     * Creates new user.
+     * @param u user to be created.
+     * @return generated id of user
+     */
     public Long create(UserCreateDTO u);
 
+    /**
+     * Finds an user by given id.
+     * @param id of user to find
+     * @return found user
+     */
     public UserDTO findById(Long id);
 
+    /**
+     * Updates user with attribute check.
+     * @param u user with new data
+     */
     public void update(UserDTO u);
-
+    
+    /**
+     * Deletes an user by given id.
+     * @param id of an user
+     */
     public void delete(Long id);
 
+    /**
+     * Returns a list of all users.
+     * @return
+     */
     public List<UserDTO> findAll();
 
+    /**
+     * Returns a list of users filtered by criteria. All parameters can be null. 
+     * If all parameters are null, a list of all users is returned.
+     * @param sex - gender of an user
+     * @param minAge - lower threshold for user age
+     * @param maxAge - upper threshold for user age
+     * @param minWeight - lower threshold for user weight
+     * @param maxWeight - upper threshold for user weight
+     * @return a list of users filtered by input parameters.
+     */
     public List<UserDTO> findByParameters(Gender sex, Integer minAge, Integer maxAge, Integer minWeight, Integer maxWeight);
 }
