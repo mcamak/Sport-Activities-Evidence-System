ad1a603cd4cc1d152a42d271fe029431d114f8eb
Marián Čamák <majocamak@gmail.com>
2015-12-05 22:22:31 +0100

    Added more bean validation

--- SAES-api/src/main/java/cz/muni/fi/pa165/dto/UserCreateDTO.java
+++ SAES-api/src/main/java/cz/muni/fi/pa165/dto/UserCreateDTO.java
@@ -4,6 +4,7 @@ import enums.Gender;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -19,15 +20,18 @@ public class UserCreateDTO {
 
     @NotNull
     @Size(min = 3, max = 50)
+    @Pattern(regexp="\\([A-zÀ-ÿ]{3,}\\s\\?\\)+")
     String name;
 
     @NotNull
     Gender sex;
 
+    @NotNull
     @Min(0)
     @Max(800)
     Integer weight;
 
+    @NotNull
     @Min(0)
     @Max(150)
     Integer age;
