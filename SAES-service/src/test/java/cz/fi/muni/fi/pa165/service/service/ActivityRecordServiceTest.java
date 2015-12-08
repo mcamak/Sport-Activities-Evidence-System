e73d3a451062f2896e8f9bfe0694d4926084c005
Jan Shýbal <jan@shybal.eu>
2015-12-08 12:13:17 +0100

    ActivityRecordServiceTest: implemented additional test methods.

--- SAES-service/src/test/java/cz/fi/muni/fi/pa165/service/service/ActivityRecordServiceTest.java
+++ SAES-service/src/test/java/cz/fi/muni/fi/pa165/service/service/ActivityRecordServiceTest.java
@@ -1,18 +1,26 @@
 package cz.fi.muni.fi.pa165.service.service;
 
+import cz.muni.fi.pa165.saes.dao.ActivityRecordDao;
+import cz.muni.fi.pa165.saes.dao.UserDao;
 import cz.muni.fi.pa165.saes.entity.ActivityRecord;
 import cz.muni.fi.pa165.saes.entity.SportActivity;
 import cz.muni.fi.pa165.saes.entity.User;
-import cz.muni.fi.pa165.service.ActivityRecordService;
-import cz.muni.fi.pa165.service.SportActivityService;
-import cz.muni.fi.pa165.service.UserService;
+import cz.muni.fi.pa165.service.ActivityRecordServiceImpl;
 import cz.muni.fi.pa165.service.mapping.ServiceConfiguration;
 import enums.Gender;
+import java.util.ArrayList;
 import java.util.List;
-import javax.inject.Inject;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 import org.mockito.MockitoAnnotations;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -24,17 +32,20 @@ import org.testng.annotations.Test;
 @ContextConfiguration(classes = ServiceConfiguration.class)
 public class ActivityRecordServiceTest extends AbstractTestNGSpringContextTests {
 
-    @Inject
-    private ActivityRecordService activityRecordService;
+    @InjectMocks
+    private ActivityRecordServiceImpl activityRecordService;
 
-    @Inject
-    private SportActivityService sportActivityService;
+    @Mock
+    private UserDao userDAO;
 
-    @Inject
-    private UserService userService;
+    @Mock
+    private ActivityRecordDao activityRecordDAO;
 
-    ActivityRecord activityRecord1;
-    ActivityRecord activityRecord2;
+    private ActivityRecord activityRecord1;
+    private ActivityRecord activityRecord2;
+    private List<ActivityRecord> activityRecordList;
+
+    private User user;
 
     @BeforeMethod
     public void setUp() {
@@ -42,25 +53,14 @@ public class ActivityRecordServiceTest extends AbstractTestNGSpringContextTests 
 
         SportActivity sportActivity = new SportActivity();
         sportActivity.setName("Running");
-        sportActivityService.create(sportActivity);
-        
+
         // FIXME it should be possible to add one user to multiple activity records
-        User user = new User();
+        user = new User();
         user.setAge(100);
         user.setName("Pepa z Depa");
         user.setPasswordHash("LOL");
         user.setSex(Gender.MALE);
         user.setWeight(56);
-
-        User user2 = new User();
-        user2.setAge(100);
-        user2.setName("Pepa z Dola");
-        user2.setPasswordHash("LOL");
-        user2.setSex(Gender.MALE);
-        user2.setWeight(56);
-
-        userService.create(user);
-        userService.create(user2);
 
         activityRecord1 = new ActivityRecord();
         activityRecord1.setActivity(sportActivity);
@@ -72,7 +72,11 @@ public class ActivityRecordServiceTest extends AbstractTestNGSpringContextTests 
         activityRecord2.setActivity(sportActivity);
         activityRecord2.setDistance(10);
         activityRecord2.setTimeSeconds(1000L);
-        activityRecord2.addUser(user2);
+        activityRecord2.addUser(user);
+
+        activityRecordList = new ArrayList<>();
+        activityRecordList.add(activityRecord1);
+        activityRecordList.add(activityRecord2);
     }
 
     /**
@@ -82,132 +86,108 @@ public class ActivityRecordServiceTest extends AbstractTestNGSpringContextTests 
     public void createTest() {
 
         activityRecordService.create(activityRecord1);
-        activityRecordService.create(activityRecord2);
-
-        // do we find righ ammount of activities? 
-        List<ActivityRecord> foundRecords = activityRecordService.findAll();
-        assertTrue(foundRecords.size() >= 2);
-
-        // do we have right activities?
-        assertTrue(foundRecords.contains(activityRecord1));
-        assertTrue(foundRecords.contains(activityRecord2));
+        verify(activityRecordDAO).create(activityRecord1);
     }
-//
-//    /**
-//     * Tests creating with null argument
-//     */
-//    @Test(expectedExceptions = IllegalArgumentException.class)
-//    public void createNullTest() {
-//        activityRecordService.create(null);
-//    }
-//
-//    /**
-//     * Tests finding by id
-//     */
-//    @Test
-//    public void findByIdTest() {
-//        activityRecordService.create(activityRecord1);
-//        boolean isRightUserFound
-//                = activityRecord1.equals(activityRecordService.findById(activityRecord1.getId()));
-//
-//        assertTrue(isRightUserFound);
-//    }
-//
-//    /**
-//     * Tests finding by null id
-//     */
-//    @Test(expectedExceptions = IllegalArgumentException.class)
-//    public void findByIdNullTest() {
-//        activityRecordService.findById(null);
-//    }
-//
-//    /**
-//     * Tests updating
-//     */
-//    @Test
-//    public void updateTest() {
-//        activityRecordService.create(activityRecord1);
-//        activityRecord1.setSex(Gender.FEMALE);
-//        activityRecordService.update(activityRecord1);
-//        User userFound = activityRecordService.findById(activityRecord1.getId());
-//
-//        assertEquals(userFound, activityRecord1);
-//    }
-//
-//    /**
-//     * Tests updating with null argument
-//     */
-//    @Test(expectedExceptions = IllegalArgumentException.class)
-//    public void updateNullTest() {
-//        activityRecordService.update(null);
-//    }
-//
-//    /**
-//     * Tests deleting
-//     */
-//    @Test
-//    public void deleteTest() {
-//
-//        activityRecordService.create(activityRecord1);
-//        Long burdnedCaloriesId = activityRecord1.getId();
-//
-//        boolean containsBeforeDelete = activityRecordService.findById(burdnedCaloriesId) != null;
-//        activityRecordService.delete(activityRecord1);
-//        boolean containsAfterDelete = activityRecordService.findById(burdnedCaloriesId) != null;
-//
-//        System.out.print("POL " + containsAfterDelete);
-//        assertNotEquals(containsBeforeDelete, containsAfterDelete);
-//    }
-//
-//    /**
-//     * Tests deleting with null argument
-//     */
-//    @Test(expectedExceptions = IllegalArgumentException.class)
-//    public void deleteNullTest() {
-//        activityRecordService.delete(null);
-//    }
-//
-//    /**
-//     * Tests finding all records
-//     */
-//    @Test
-//    public void findAllTest() {
-//        activityRecordService.create(activityRecord1);
-//        activityRecordService.create(activityRecord2);
-//
-//        // do we find righ ammount of activities?
-//        List<User> foundRecordsList = activityRecordService.findAll();
-//        assertTrue(foundRecordsList.size() >= 2);
-//
-//        // do we have right activities?
-//        assertTrue(foundRecordsList.contains(activityRecord1));
-//        assertTrue(foundRecordsList.contains(activityRecord2));
-//    }
 
     /**
-     * Tests finding by parameters
+     * Tests creating with null argument
      */
-//    @Test
-//    public void findByParametersTest() {
-//        userService.create(activityRecord1);
-//        userService.create(activityRecord2);
-//
-//        UserFilter userFilter = new UserFilter();
-//        userFilter.setMinAge(activityRecord1.getAge() - 1);
-//        userFilter.setMaxAge(activityRecord2.getAge() + 1);
-//        
-//        System.out.println("BEKL "+userService.findByParameters(userFilter).size());
-//
-//        assertTrue(userService.findByParameters(userFilter).size() == 2);
-//
-//    }
-    // TODO
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void createNullTest() {
+        Mockito.doThrow(new IllegalArgumentException()).when(activityRecordDAO).create(null);
+        activityRecordService.create(null);
+    }
+
+    /**
+     * Tests finding by id
+     */
+    @Test
+    public void findByIdTest() {
+        Long id = 5154L;
+        when(activityRecordDAO.findActivityRecord(id)).thenReturn(this.activityRecord1);
+
+        ActivityRecord result = activityRecordService.findById(id);
+        verify(activityRecordDAO).findActivityRecord(id);
+        verifyNoMoreInteractions(activityRecordDAO);
+
+        assertEquals(result, activityRecord1);
+    }
+
+    /**
+     * Tests finding by null id
+     */
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void findByIdNullTest() {
+        Mockito.doThrow(new IllegalArgumentException()).when(activityRecordDAO).findActivityRecord(null);
+        activityRecordService.findById(null);
+    }
+
+    /**
+     * Tests updating
+     */
+    @Test
+    public void updateTest() {
+        activityRecordService.update(activityRecord1);
+        verify(activityRecordDAO).update(activityRecord1);
+    }
+
+    /**
+     * Tests updating with null argument
+     */
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void updateNullTest() {
+        Mockito.doThrow(new IllegalArgumentException()).when(activityRecordDAO).update(null);
+        activityRecordService.update(null);
+    }
+
+    /**
+     * Tests deleting
+     */
+    @Test
+    public void deleteTest() {
+        activityRecordService.delete(activityRecord1);
+        verify(activityRecordDAO).delete(activityRecord1);
+    }
+
+    /**
+     * Tests deleting with null argument
+     */
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void deleteNullTest() {
+        Mockito.doThrow(new IllegalArgumentException()).when(activityRecordDAO).delete(null);
+        activityRecordService.delete(null);
+    }
+
     /**
      * Tests finding all records
      */
-//    @Test
-//    public void authenticateTest() {
-//        userService.create(activityRecord1);
-//        assertTrue(userService.authenticate(activityRecord1.getId(), activityRecord1.getPasswordHash()));
-//    }
+    @Test
+    public void findAllTest() {
+        when(activityRecordDAO.findAll()).thenReturn(activityRecordList);
+
+        List<ActivityRecord> result = activityRecordService.findAll();
+        assertEquals(result.size(), 2);
+        assertTrue(result.contains(activityRecord1));
+        assertTrue(result.contains(activityRecord2));
+    }
+
+    /**
+     * Tests removing user from record
+     */
+    @Test
+    public void removeUserFromActivityRecordTest() {
+        when(activityRecordDAO.findActivityRecord(activityRecord1.getId())).thenReturn(activityRecord1);
+        when(userDAO.findUser(user.getId())).thenReturn(user);
+
+        Integer usersBeforeRemoval = activityRecord1.getUsers().size();
+        activityRecordService.removeUserFromActivityRecord(user.getId(), activityRecord1.getId());
+        Integer usersAfterRemoval = activityRecord1.getUsers().size();
+
+        assertTrue(usersAfterRemoval == (usersBeforeRemoval - 1));
+
+        verify(activityRecordDAO).findActivityRecord(user.getId());
+        verify(userDAO).findUser(user.getId());
+        verify(activityRecordDAO).update(activityRecord1);
+    }
+
 }
