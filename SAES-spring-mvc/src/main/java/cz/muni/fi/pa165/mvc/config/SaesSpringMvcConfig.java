286041431de97740999b48367dd2c8d215ea9eec
Marián Čamák <majocamak@gmail.com>
2015-12-20 18:32:04 +0100

    Added mvc config classes

---
+++ SAES-spring-mvc/src/main/java/cz/muni/fi/pa165/mvc/config/SaesSpringMvcConfig.java
@@ -0,0 +1,42 @@
+package cz.muni.fi.pa165.mvc.config;
+
+import cz.muni.fi.pa165.data.SaesDataConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+/**
+ * Created by Marian Camak on 20. 12. 2015.
+ */
+@Configuration
+@EnableWebMvc
+@Import({SaesDataConfiguration.class})
+@ComponentScan(basePackages = "cz.muni.fi.pa165.mvc.controllers")
+public class SaesSpringMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    @Bean
+    public Validator validator() {
+        return new LocalValidatorFactoryBean();
+    }
+}
