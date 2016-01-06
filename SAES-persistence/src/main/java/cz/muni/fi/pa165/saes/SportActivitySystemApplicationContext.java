70541b9a270e2f395fbf7a2260940d6ecc6b9cea
Marián Čamák <majocamak@gmail.com>
2016-01-06 22:09:04 +0100

    Fixed bug when tests weren't passing

--- SAES-persistence/src/main/java/cz/muni/fi/pa165/saes/SportActivitySystemApplicationContext.java
+++ SAES-persistence/src/main/java/cz/muni/fi/pa165/saes/SportActivitySystemApplicationContext.java
@@ -5,21 +5,17 @@ import org.hibernate.jpa.HibernatePersistenceProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.instrument.classloading.InstrumentationLoadTimeWeaver;
-import org.springframework.instrument.classloading.LoadTimeWeaver;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import javax.sql.DataSource;
 
 /**
- *
+ * Configuration for application.
  * @author Marian Camak
  */
 @Configuration
@@ -29,37 +25,23 @@ import javax.sql.DataSource;
 public class SportActivitySystemApplicationContext {
 
     @Bean
-    public PersistenceExceptionTranslationPostProcessor postProcessor() {
-        return new PersistenceExceptionTranslationPostProcessor();
-    }
-
-    @Bean
     public JpaTransactionManager transactionManager() {
         return new JpaTransactionManager(entityManagerFactory().getObject());
     }
 
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-        LocalContainerEntityManagerFactoryBean jpaFactoryBean = new LocalContainerEntityManagerFactoryBean();
-        jpaFactoryBean.setDataSource(db());
-        jpaFactoryBean.setLoadTimeWeaver(instrumentationLoadTimeWeaver());
-        jpaFactoryBean.setPersistenceProviderClass(HibernatePersistenceProvider.class);
-        return jpaFactoryBean;
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean =
+                new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(this.dataSource());
+        entityManagerFactoryBean.setPersistenceProvider(new HibernatePersistenceProvider());
+        return entityManagerFactoryBean;
     }
 
     @Bean
-    public LocalValidatorFactoryBean localValidatorFactoryBean() {
-        return new LocalValidatorFactoryBean();
-    }
-
-    @Bean
-    public LoadTimeWeaver instrumentationLoadTimeWeaver() {
-        return new InstrumentationLoadTimeWeaver();
-    }
-
-    @Bean
-    public DataSource db() {
-        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-        return builder.setType(EmbeddedDatabaseType.DERBY).build();
+    public DataSource dataSource() {
+        final EmbeddedDatabaseBuilder databaseBuilder = new EmbeddedDatabaseBuilder();
+        databaseBuilder.setType(EmbeddedDatabaseType.DERBY);
+        return databaseBuilder.build();
     }
 }
