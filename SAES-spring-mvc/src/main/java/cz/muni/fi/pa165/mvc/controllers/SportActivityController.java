1a748716c198098d334582169bfcae4d1183be87
Barborka <Barborka@Barborka-NB>
2016-01-09 10:50:55 +0100

    SportActivityController fixed some bugs.

--- SAES-spring-mvc/src/main/java/cz/muni/fi/pa165/mvc/controllers/SportActivityController.java
+++ SAES-spring-mvc/src/main/java/cz/muni/fi/pa165/mvc/controllers/SportActivityController.java
@@ -1,7 +1,10 @@
+package cz.muni.fi.pa165.mvc.controllers;
+
+
 import cz.muni.fi.pa165.dto.SportActivityCreateDTO;
+import cz.muni.fi.pa165.exceptions.EntityReferenceException;
 import cz.muni.fi.pa165.facade.SportActivityFacade;
-import cz.muni.fi.pa165.service.facade.SportActivityFacadeImpl;
-import cz.muni.fi.pa165.service.mapping.ServiceConfiguration;
+//import cz.muni.fi.pa165.service.mapping.ServiceConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Controller;
@@ -15,42 +18,81 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.validation.Valid;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 /**
  *
  * @author Barbora B.
  */
 
 @Controller
-@Import({ServiceConfiguration.class})
 @RequestMapping("/sportActivity")
 public class SportActivityController {
     
     @Autowired
-    private SportActivtyFacade sportActivityFacade = new SportActivityFacadeImpl();
-
+    private SportActivityFacade sportActivityFacade;
+    
+    /**
+     * Prepares an empty form.
+     *
+     * @param model data to be displayed
+     * @return JSP page
+     */
     @RequestMapping(value = "/new", method = RequestMethod.GET)
     public String newSportActivity(Model model) {
         model.addAttribute("sportActivityCreate", new SportActivityCreateDTO());
         return "sportActivity/new";
     }
-
+    
+    
+     /**
+     * Creates new sport activity
+     * @param model data to display
+     * @return JSP page
+     */
     @RequestMapping(value = "/create", method = RequestMethod.POST)
     public String create(@Valid @ModelAttribute("sportActivityCreate") SportActivityCreateDTO formBean, BindingResult bindingResult,
                          Model model, RedirectAttributes redirectAttributes, UriComponentsBuilder uriBuilder) {
+        
+        if (bindingResult.hasErrors()) {
+            for (ObjectError ge : bindingResult.getGlobalErrors()) {
+                model.addAttribute(ge.getObjectName() + "_error", true);
+            }
+            for (FieldError fe : bindingResult.getFieldErrors()) {
+                model.addAttribute(fe.getField() + "_error", true);
+            }
+            return "sportActivity/new";
+        }
         
         Long id = sportActivityFacade.create(formBean);
         redirectAttributes.addFlashAttribute("alert_info", "Sport Activity " + id + " was created");
         return "redirect:" + uriBuilder.path("/sportActivity").toUriString();
     }
     
+    
+    /**
+     * List of sport activities.
+     *
+     * @param model data to display
+     * @return JSP page name
+     */
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     public String list(Model model) {
-        model.addAttribute("sportActivities", SportActivityFacade.findAll());
+        model.addAttribute("sportActivities", sportActivityFacade.findAll());
         return "sportActivity/list";
     }
     
+    
+    /**
+     * Delete sport activity with id.
+     *
+     * @param id             
+     * @param uriBuilder
+     * @param redirectAttributes
+     * @return JSP page
+     */
     @RequestMapping(value = "/remove/{id}", method = RequestMethod.POST)
-    public String remove(@PathVariable("id") long activityId, UriComponentsBuilder uriBuilder, RedirectAttributes redirectAttributes) {
+    public String remove(@PathVariable long activityId, UriComponentsBuilder uriBuilder, RedirectAttributes redirectAttributes) throws EntityReferenceException {
         sportActivityFacade.delete(activityId);
         redirectAttributes.addFlashAttribute("alert_info", "SportActivity " + activityId + " was deleted.");
         return "redirect:" + uriBuilder.path("/sportActivity").toUriString();
