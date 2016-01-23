7e5d135a75271bf0a193c273eb9d1061d525c6da
Marián Čamák <majocamak@gmail.com>
2016-01-23 17:22:18 +0100

    Added javadoc

--- SAES-rest/src/main/java/cz/muni/fi/pa165/rest/controllers/MainController.java
+++ SAES-rest/src/main/java/cz/muni/fi/pa165/rest/controllers/MainController.java
@@ -1,7 +1,5 @@
 package cz.muni.fi.pa165.rest.controllers;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,8 +15,12 @@ import java.util.Map;
 @RestController
 public class MainController {
 
-    final static Logger logger = LoggerFactory.getLogger(MainController.class);
-
+    /**
+     * The main entry point of the REST API
+     * Provides access to all the resources with links to resource URIs
+     *
+     * @return resources uris
+     */
     @RequestMapping(value = "/", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public final Map<String, String> getResources() {
 
